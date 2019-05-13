a19c8fc10b4fe41b2b56ffa832e1233ccda16f7f
Žan Mikola <zan.mikola@student.fmf.uni-lj.si>
2019-05-13 18:34:47 +0200

    dodelal tekstovni vmesnik

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -1,11 +1,18 @@
-from model import *
+import model
 
-def izpis_igre():
+def izpis_igre(igra):
     return '''stevilo preostalih poskusov: {}, do zdaj napacne crke: {}, pravilni del gesla: {}
-    '''.format(STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napacnih(), igra.napacne_crke(), igra.pravilni_del_gesla())
+    '''.format(model.STEVILO_DOVOLJENIH_NAPAK - igra.stevilo_napacnih(), igra.napacne_crke(), igra.pravilni_del_gesla())
 
-def izpis_zmage():
+def izpis_zmage(igra):
     return '''Čestitam uganil si {}'''.format(igra.geslo())
 
-def izpis_poraza():
+def izpis_poraza(igra):
     return '''Žal nisi ugotovil gesla {}'''.format(igra.geslo())
+
+def zahtevaj_vnos(igra):
+    return input('Vnesi črko: ')
+
+def poženi_vmesnik():
+    igra = model.nova_igra()
+    
